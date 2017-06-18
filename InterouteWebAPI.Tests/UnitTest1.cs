08a6ee3b1ce0cad3576c7d83ca8a53f57cae8f8f
Lynnsay Piper <lynnsaymay.piper@outlook.com>
2017-06-18 15:16:18 +0100

    Add project files.

---
+++ InterouteWebAPI.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace InterouteWebAPI.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
